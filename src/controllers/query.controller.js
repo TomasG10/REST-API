c77b339a10a74e2cd1c5695d5eb93a7331a5072e
TomasG10 <94805682+TomasG10@users.noreply.github.com>
2023-05-13 01:45:41 -0600

    delete

---
+++ src/controllers/query.controller.js
@@ -0,0 +1,14 @@
+import { getConnection } from "../database/connnection";
+
+
+export const getProducers = async (req, res) => {
+
+    const pool = await getConnection();
+    const result = await pool.request().query('SELECT * FROM producers');
+    res.json(result.recordset);
+
+};
+
+export const createNewProducer = async (req, res) => {
+    res.json('New Producer');
+}
